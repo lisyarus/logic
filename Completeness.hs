46dd80e7f927ed8e552832be9aacd526da66f9b0
lisyarus <lisyarus@gmail.com>
2013-07-31 21:42:36 +0400

    Checking formula's validness + gitignore updated

--- Completeness.hs
+++ Completeness.hs
@@ -1,1 +1,35 @@
 module Completeness where
+
+import Expression
+import Data.List
+
+getVariables :: Expression -> [String]
+getVariables expr = nub (getVariables' expr)
+    where getVariables' expr =
+            case expr of
+                Variable name -> [name]
+                Negation a -> getVariables a
+                Implication a b -> (getVariables a) ++ (getVariables b)
+                Conjunction a b -> (getVariables a) ++ (getVariables b)
+                Disjunction a b -> (getVariables a) ++ (getVariables b)
+
+data VariableValue = VariableValue String Bool
+    deriving(Show)
+
+getVariableValue name varlist =
+    case head varlist of
+        VariableValue name' val -> if name == name' then val else getVariableValue name (tail varlist)
+
+calculate :: Expression -> [VariableValue] -> Bool
+calculate (Variable name) varlist = getVariableValue name varlist
+calculate (Negation e) varlist = not $ calculate e varlist
+calculate (Implication a b) varlist = (not $ calculate a varlist) || (calculate b varlist)
+calculate (Conjunction a b) varlist = (calculate a varlist) && (calculate b varlist)
+calculate (Disjunction a b) varlist = (calculate a varlist) || (calculate b varlist)
+
+generateAllVariants :: [String] -> [[VariableValue]]
+generateAllVariants [] = []
+generateAllVariants [name] = [[VariableValue name True], [VariableValue name False]]
+generateAllVariants (name:tail) =
+    let tailVariants = generateAllVariants tail in
+    (map (\ values -> (VariableValue name True):values) tailVariants) ++ (map (\ values -> (VariableValue name False):values) tailVariants)
--- CompletenessMain.hs
+++ CompletenessMain.hs
@@ -1,3 +1,15 @@
 module Main where
 
-main = putStrLn ""
+import Expression
+import Completeness
+
+do_main :: String -> String
+do_main str =
+    let expression = parse str
+        variables = getVariables expression in
+    if foldl (&&) True $ map (calculate expression) (generateAllVariants variables) then
+        "Need a proof of this..."
+    else
+        error "The formula is not logically valid!"
+
+main = interact ((\l -> l ++ "\n") . do_main . head . lines)
